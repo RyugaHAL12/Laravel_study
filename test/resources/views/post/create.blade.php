b961bb53f19be214d0a3304699e9e497168052ab
ryugahal12 <ryugahal12@gmail.com>
2023-08-24 10:54:01 +0900

    投稿のフォームを表示させるところまで。保存の設定はしてない。

---
+++ test/resources/views/post/create.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Post') }}
+        </h2>
+    </x-slot>
+	<div class="max-w-7xl mx-auto px-6">
+		<form>
+			<div class="mt-8">
+				<div class="w-full flex flex-col">
+					<label for="title" class="font-semibold mt-4 text-white">件名</label>
+					<input type="text" name="title" class="w-auto py-2 bg-gray-500 border border-white rounded-md text-white" id="title">
+				</div>
+			</div>
+
+			<div class="w-full flex flex-col">
+				<label for="body" class="font-semibold mt-4 text-white">本文</label>
+				<textarea name="body" class="w-auto py-2 bg-gray-500 border border-white rounded-md text-white" id="body" cols="30" rows="5"></textarea>
+			</div>
+
+			<x-primary-button class="mt-4">送信する</x-primary-button>
+		</form>
+	</div>
+</x-app-layout>
