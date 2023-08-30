ce6dd9de207402263c4b320d4420305ddca8339d
RyugaHAL12 <ryugahal12@gmail.com>
2023-08-30 11:29:26 +0900

    リッチテキストを追加する途中。

--- test/app/Http/Controllers/ArticleController.php
+++ test/app/Http/Controllers/ArticleController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Article;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     //
 	public function create(){
+		//	リッチテキストエディタに戻る
 		return view('article.create');
+	}
+
+	public function store(Request $request){
+		//	記事のモデルのインスタンスを作成
+		$article = new Article();
+		
+		//	fill()：モデルの複数のカラムを更新することができる。
+		$article->fill([
+			'title'=>request('title'),
+			'body'=>request('body')
+		]);
+
+		//	データベースへの保存
+		$article->save();
+
+		//	記事作成の画面にメッセージ付きで返す。
+		return back()->with('message','Article Submit is Success');
 	}
 }
